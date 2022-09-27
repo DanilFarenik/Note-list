d2e125da2a501cec60e5f11dcd378873c122770d
Farenyk Danyl <lfybl.ua@gmail.com>
2022-09-27 15:39:08 +0300

    Added a job with localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import noteReducer from "./noteSlice";
 import modalReducer from "./modalSlice";
 
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    modal: modalReducer
-  }
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  notes: noteReducer,
+  modal: modalReducer
 })
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+
 export default store;
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
